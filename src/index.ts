6643fa56ff4cee7995b9496f6660dfd2872f8543
ShinChven <shinchven@gmail.com>
2022-11-21 00:47:11 +0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// export * from './module';
+import {exec} from 'child_process';
+
+const ffprobe = async (filepath: string) => {
+  return new Promise((resolve, reject) => {
+    exec(`ffprobe -v quiet -print_format json -show_format -show_streams "${filepath}"`, (err, stdout) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(JSON.parse(stdout));
+      }
+    });
+  });
+}
